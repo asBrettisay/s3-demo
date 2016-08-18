f06af61f355c067765b03e71e5647cde59d34c24
asBrettisay <brett.caudill.is@gmail.com>
2016-08-18 13:06:56 -0600

    Removed the work I did.

--- public/app.js
+++ public/app.js
@@ -1,38 +1,7 @@
 angular.module('s3Demo', [])
 .directive('fileRead', function() {
-  return {
-    restrict: 'A',
-    controller: function($scope, dataService) {
-      $scope.storeImage = dataService.storeImage;
-    },
-    link: function(scope, elem, attrs) {
-      elem.bind('change', function(changeEvent) {
-        var reader = new FileReader();
-        reader.onload = function(loadEvent) {
-          var fileread = loadEvent.target.result;
-          scope.storeImage(fileread, 'test');
-        }
 
-        // Convert image into base64
-        // Result will be avaiable in target.Result
-        // Triggers onload event when finished
-        reader.readAsDataURL(changeEvent.target.files[0]);
-      })
-    }
-  }
 })
 .service('dataService', function($http) {
-  this.storeImage = function(imageData, fileName) {
-    var imageExtension = imageData.split(';')[0].split('/')
-    imageExtension = imageExtension[imageExtension.length - 1];
 
-    var newImage = {
-      imageName: fileName,
-      imageBody: imageData,
-      imageExtension: imageExtension,
-      userEmail: 'brett.caudill.is@gmail.com'
-    }
-
-    return $http.post('/api/newimage', newImage);
-  }
 })
